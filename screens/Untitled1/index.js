7ab15a9ae84156c211cfcead65d0342b6a951635
Daniel Palacios <daniel80380@gmail.com>
2022-06-07 16:02:24 -0300

    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -17,6 +17,15 @@ const Untitled1 = () => {
       backgroundColor: "#E4E4E4",
       borderRadius: 0,
       color: "#777777"
+    }}></View><View style={{
+      left: 24,
+      top: 154,
+      position: "absolute",
+      height: 60,
+      width: 140,
+      backgroundColor: "#E4E4E4",
+      borderRadius: 0,
+      color: "#777777"
     }}></View></View>;
 };
 
