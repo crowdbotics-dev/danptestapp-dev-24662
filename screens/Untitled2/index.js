3ea2b5049b4cfa7c56987af6fb6afaa1826fe35e
Daniel Palacios <daniel80380@gmail.com>
2022-06-27 17:28:45 -0300

    Crowdbotics Studio: Update app screens

--- screens/Untitled2/index.js
+++ screens/Untitled2/index.js
@@ -3,10 +3,10 @@ import { View } from "react-native";
 
 const Untitled2 = () => {
   return <View style={{
-    backgroundColor: '#f0f0f1',
+    backgroundColor: "#6f6feb",
     padding: 10,
-    position: 'relative',
-    height: '100%'
+    position: "relative",
+    height: "100%"
   }}>
       </View>;
 };
