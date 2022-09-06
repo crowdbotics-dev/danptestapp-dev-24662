fb94d95d2ae4fe7eb2353dc7e47d1fd2d6076093
Daniel Palacios <daniel80380@gmail.com>
2022-09-06 15:40:44 -0300

    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled4/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Untitled4 = () => {
+  return <View style={_styles.srqxVkvF}>
+      </View>;
+};
+
+export default Untitled4;
+
+const _styles = StyleSheet.create({
+  srqxVkvF: {
+    backgroundColor: "#2424d9",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  }
+});
