d19bc1f84d4a260ef4fcd0fd3c2fb1c2573287de
Daniel Palacios <daniel80380@gmail.com>
2022-09-12 11:32:16 -0300

    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,18 +1,27 @@
+import { Switch } from "react-native";
 import React from "react";
 import { View, StyleSheet } from "react-native";
 
 const Untitled1 = () => {
-  return <View style={_styles.XeVTcsGx}>
-      </View>;
+  return <View style={_styles.YXnLVSOm}>
+      <Switch style={_styles.KdhUFSSq} thumbColor="#da3737"></Switch></View>;
 };
 
 export default Untitled1;
 
 const _styles = StyleSheet.create({
-  XeVTcsGx: {
+  YXnLVSOm: {
     backgroundColor: "#f0f0f1",
     padding: 10,
     position: "relative",
     height: "100%"
+  },
+  KdhUFSSq: {
+    left: 121.08908379653485,
+    top: 144.63418342363886,
+    position: "absolute",
+    width: 50,
+    height: 25,
+    transform: "rotate(90deg)"
   }
 });
